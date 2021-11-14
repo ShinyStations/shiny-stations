bfe85ad01e38ec2af102a40980db2911d23c9376
Sushant Kumar <sushantkumar23@gmail.com>
2021-11-14 12:24:01 +0530

    Adding basic stuff

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,209 @@
 import Head from 'next/head'
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+  CloudUploadIcon,
+  CogIcon,
+  LockClosedIcon,
+  MenuIcon,
+  RefreshIcon,
+  ServerIcon,
+  ShieldCheckIcon,
+  XIcon,
+} from '@heroicons/react/outline'
+import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid'
 
-export default function Home() {
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'Enter the Station', href: '#' },
+  { name: 'Roadmap', href: '#' },
+  { name: 'About', href: '#' },
+]
+
+export default function Example() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-white">
       <Head>
-        <title>Create Next App</title>
+        <title>Shiny Stations</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="relative overflow-hidden">
+        <Popover as="header" className="relative">
+          <div className="pt-6 bg-gray-900">
+            <nav
+              className="relative flex items-center justify-between px-4 mx-auto max-w-7xl sm:px-6"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="w-auto h-8 sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="flex items-center -mr-2 md:hidden">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden space-x-8 md:flex md:ml-10">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-white hover:text-gray-300"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+                <a
+                  href="#"
+                  className="inline-flex items-center px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md hover:bg-gray-700"
+                >
+                  Connect Wallet
+                </a>
+              </div>
+            </nav>
+          </div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            <Popover.Panel focus className="absolute inset-x-0 top-0 p-2 transition origin-top transform md:hidden">
+              <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="w-auto h-8"
+                      src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="px-2 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="px-5 mt-6">
+                    <a
+                      href="#"
+                      className="block w-full px-4 py-3 font-medium text-center text-white rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
+                    >
+                      Connect Wallet
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+        <main>
+          <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+            <div className="mx-auto max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="max-w-md px-4 mx-auto sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                  <div className="lg:py-24">
+                    <a
+                      href="#"
+                      className="inline-flex items-center p-1 pr-2 text-white bg-black rounded-full sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
+                    >
+                      <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">
+                        Launching Soon
+                      </span>
+                      <span className="ml-4 text-sm">in 3 days</span>
+                      <ChevronRightIcon className="w-5 h-5 ml-2 text-gray-500" aria-hidden="true" />
+                    </a>
+                    <h1 className="mt-4 text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                      <span className="block">Welcome to the</span>
+                      <span className="block pb-3 text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
+                        Shiny Stations
+                      </span>
+                    </h1>
+                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                      Our goal is simple, we want to create a place that everyone in the Metaverse can join, while maintaining a project that grows in size and utility for years to come.
+                    </p>
+                    <div className="mt-10 sm:mt-12">
+                      <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
+                        <div className="sm:flex">
+                          <div className="mt-3 sm:mt-0">
+                            <button
+                              type="submit"
+                              className="block w-full px-4 py-3 font-medium text-white rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                            >
+                              Connect wallet
+                            </button>
+                          </div>
+                        </div>
+                        <p className="mt-3 text-sm text-gray-300 sm:mt-4">
+                        Our first and foremost goal is deliver a wonderful product to those in The Tower - NFT Galleries and Common Room spaces{' '}
+                          <a href="#" className="font-medium text-white">
+                            Terms of service
+                          </a>
+                          .
+                        </p>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          {/* Feature section with screenshot */}
+          <div className="relative py-16 bg-gray-50 sm:py-24 lg:py-32">
+            <div className="max-w-md px-4 mx-auto text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+              <div>
+                <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">Our Vision</h2>
+                <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Explore the Stations
+                </p>
+                <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
+                  Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
+                  malesuada. Eleifend condimentum id viverra nulla.
+                </p>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer className="bg-gray-50" aria-labelledby="footer-heading">
+          <h2 id="footer-heading" className="sr-only">
+            Footer
+          </h2>
+          <div className="max-w-md px-4 mx-auto sm:max-w-7xl sm:px-6 lg:px-8">
+            <div className="py-8 mt-12 border-t border-gray-200">
+              <p className="text-base text-gray-400 xl:text-center">&copy; 2021 Shiny Stations, Inc. All rights reserved.</p>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
