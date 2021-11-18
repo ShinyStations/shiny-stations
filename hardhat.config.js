b7b77338cbbb89c56757db5ad13a51870286ba1e
mrhouzlane <75360886+mrhouzlane@users.noreply.github.com>
2021-11-19 00:25:21 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,24 @@
-require('@nomiclabs/hardhat-waffle')
-const fs = require('fs')
-const privateKey = fs.readFileSync('.secret').toString()
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "b7cb080783d942d88ecd4787eb56d0d9"
 
-const projectId = process.env.INFURA_PROJECT_ID
 
 module.exports = {
-  networks: {
+  networks : {
     hardhat: {
-      chainId: 1337,
+      chainId: 1337
     },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
+    rinkeby: {
+      url : `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts : [privateKey]
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
-    },
+      url : `https://mainnet.infura.io/v3/${projectId}`,
+      accounts : [privateKey]
+    }
+
   },
-  solidity: '0.8.4',
-}
+  solidity: "0.8.4",
+};
+
