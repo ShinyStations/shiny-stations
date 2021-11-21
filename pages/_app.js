82e7f12715e8851c93c779098167ab7e5b75cfbe
mrhouzlane <75360886+mrhouzlane@users.noreply.github.com>
2021-11-21 20:51:30 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import 'tailwindcss/tailwind.css'
+import Link from "next/link"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+function UNLOCK({ Component, pageProps }) {
+  return (
+  
+    <div> 
+          
+         
+
+    <Component {...pageProps} />
+
+    </div> 
+  
+)
 }
-
-export default MyApp
+    
+export default UNLOCK
